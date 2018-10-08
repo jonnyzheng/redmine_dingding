0c9bbfa99492604cc19b5b1637f5b42bfef066e0
Jonny Zheng <zhengyi@zz-med.com>
2018-10-08 20:05:02 +0800

    init repository

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_dingding do
+  name 'Redmine Dingding plugin'
+  author 'Jonny Zheng'
+  description 'Dingding notification for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
